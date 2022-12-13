e4102c617f39f563019fe4504feb8e3b6d50a8da
Guddy Tech <guddytechs@gmail.com>
2022-12-12 16:00:56 -0800

    added 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"     
- * Return: Always 0
+ *
+ * Return: always 0
  */
 int main(void)
 {
